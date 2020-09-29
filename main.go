dc3d925d8abfa299d993c37441d5e21947203f31
Alejandro Silva <jose.silva5468@alumnos.udg.mx>
2020-09-29 12:46:04 -0500

    codigo del numero e agregado

--- main.go
+++ main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func factorial(numero float64) float64 {
+
+	if numero == 0 {
+		return 1
+	} else {
+		return numero * factorial(numero-1)
+	}
+}
+
+func main() {
+	var n float64
+	var i float64 = 1.0
+	var suma float64 = 1.0
+
+	fmt.Scanln(&n)
+
+	for i <= n {
+		suma += 1 / factorial(i)
+		i++
+	}
+
+	fmt.Println(suma)
 }
